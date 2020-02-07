48af547cbfe15c82e68af5cc4e18972bdd40fb6f
Beth Ruff <beth.ruff2@gmail.com>
2020-02-07 00:34:31 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,9 +1,19 @@
 class Cat 
   attr_accessor :name 
   
+  def speak(meow)
+    @speak=(meow)
+  end
+  
+  def speak
+    @speak
+  end
+  
 end
 
 maru = Cat.new
 maru.name = "Maru"
 maru.name 
+
+maru.speak = meow
 
